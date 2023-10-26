8ca09fec2ca3f58aceead38899dac522f8f48487
QobiljonIbaydullayevDev <qobiljonyoqubovich2002@gmail.com>
2023-10-26 22:21:31 +0500

    Edited style.css file

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const todoList = document.querySelector('.newTodo')
 
 const addHandler=()=>{
     if(inputBx.value===''){
-        alert("you must write anything")
+        alert("you must write anything !!!")
     }
 
     else{
         let newTodoList = document.createElement("li")
         newTodoList.innerHTML=inputBx.value
         todoList.appendChild(newTodoList)
+
         let span =document.createElement("span")
         span.innerHTML="Remove"
         todoList.appendChild(span)
@@ -19,5 +20,13 @@ const addHandler=()=>{
         })
     }
     inputBx.value=''
+}
+
+const focusHandler=()=>{
+    inputBx.style.backgroundColor="Azure"
+    inputBx.style.outline="None"
+    // inputBx.focus()
+    // inputBx.autocapitalize="off"
+    // inputBx.style.border="None"
 }
 
